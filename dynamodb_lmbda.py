4ab8916606e10f88bfe3ea85c67ddc3fef5d0991
explorewithsupriya <125638840+explorewithsupriya@users.noreply.github.com>
2023-02-23 20:33:09 +0530

    Update dynamodb_lmbda.py
    adding query and delete functions

--- dynamodb_lmbda.py
+++ dynamodb_lmbda.py
@@ -4,8 +4,10 @@
 import boto3
 import os
 
+
 def lambda_handler(event, context):
-    return updateMyData(event['timestamps'], event['sensor_data'])
+    return deleteMyData(event['timestamps'])
+
 
 def insertInMyData(timestamps, sensor_data, dyn_resource=None):
 
@@ -13,7 +15,8 @@ def insertInMyData(timestamps, sensor_data, dyn_resource=None):
         dyn_resource = boto3.resource('dynamodb')
 
     table = dyn_resource.Table('MyData2')
-    return table.put_item(Item={'timestamps': timestamps, 'sensor_data': sensor_data})
+    return table.put_item(Item={'timestamps': timestamps,
+                          'sensor_data': sensor_data})
 
 
 def updateMyData(timestamps, sensor_data, dyn_resource=None):
@@ -25,7 +28,23 @@ def updateMyData(timestamps, sensor_data, dyn_resource=None):
     return table.update_item(Key={'timestamps': timestamps},
                              UpdateExpression='set sensor_data=:d',
                              ExpressionAttributeValues={':d': sensor_data},
-                             ReturnValues="UPDATED_NEW")
+                             ReturnValues='UPDATED_NEW')
 
 
+def queryMyData(timestamps, dyn_resource=None):
 
+    if dyn_resource is None:
+        dyn_resource = boto3.resource('dynamodb')
+
+    table = dyn_resource.Table('MyData2')
+    return table.query(KeyConditionExpression=boto3.dynamodb.conditions.Key('timestamps'
+                       ).eq(timestamps))
+
+
+def deleteMyData(timestamps, dyn_resource=None):
+
+    if dyn_resource is None:
+        dyn_resource = boto3.resource('dynamodb')
+
+    table = dyn_resource.Table('MyData2')
+    return table.delete_item(Key={'timestamps': timestamps})
